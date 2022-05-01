5a2c9762fff4ab322d3585a04d9687d978869adb
Peter Wagner <peter.wagner1986@gmail.com>
2022-05-01 10:36:26 +0200

    changed some code

--- code/kiste.js
+++ code/kiste.js
@@ -1,14 +1,15 @@
 const RESULT = 25 + 4 + 1987 + 8 + 5 + 1990;
 const enterBtn = document.querySelector('[data-enter-btn]');
 const input = document.querySelector('input');
-const resultText = document.querySelector('[data-message] p');
 const message = document.querySelector('[data-message]');
+const counter = document.querySelector('[data-counter]');
+const resultText = document.querySelector('[data-text]');
 const gameState = document.querySelector('[data-game-state]');
 
 let i = 90;
 
 let timer = setInterval(() => {
-    resultText.innerHTML = `Übrigens....ihr händ nu na <br /> ${i} Sekunde <br /> Ziit.`;
+    counter.innerHTML = `Übrigens....ihr händ nu na <br /> ${i} Sekunde <br /> Ziit.`;
     if(i > 0){
         i--;
     }else{
@@ -24,20 +25,27 @@ let timer = setInterval(() => {
 
 input.focus();
 
-enterBtn.addEventListener('click', (e) => {
-    if(input.value === RESULT.toString()){
-        clearInterval(timer);
-        setTimeout(() => {
-            gameState.innerHTML = `
-                <h1>Gratuliere<br /> ihr händ s Rätsel glöst</h1>
-                <img src="../design/treasure.gif">
-            `;
-            gameState.classList.add('show');
-        }, 1000)
-    }else{
-        resultText.textContent = 'Das isch leider falsch.';
-        input.value = '';
-        input.focus();
-    };
+window.addEventListener('keydown', (e) => {
+
+    switch(e.key){
+        case "Enter":
+            if(input.value === RESULT.toString()){
+                clearInterval(timer);
+                resultText.textContent = 'Richtig!!!'
+                setTimeout(() => {
+                    gameState.innerHTML = `
+                        <h1>Gratuliere<br /> ihr händ s Rätsel glöst</h1>
+                        <img src="../design/treasure.gif">
+                    `;
+                    gameState.classList.add('show');
+                }, 2000)
+            }else{
+                resultText.textContent = 'Das isch leider falsch.';
+                input.value = '';
+                input.focus();
+            };
+        default:
+            return
+    }
 });
 
